1136df9920b68ca860698fede6a0fe2eb94fd67c
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-04-28 23:50:54 +0200

    💚 Add missing log function in tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/6.0/alpine/test/docker_test.sh
+++ images/6.0/alpine/test/docker_test.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+log() {
+    echo "[$0] [$(date +%Y-%m-%dT%H:%M:%S)] $*"
+}
+
 ################################################################################
 # Testing docker containers
 
-echo "Waiting to ensure everything is fully ready for the tests..."
+log "Waiting to ensure everything is fully ready for the tests..."
 sleep 60
 
-echo "Checking main containers are reachable..."
+log "Checking main containers are reachable..."
 if ! ping -c 10 -q "${DOCKER_TEST_CONTAINER}" ; then
-    echo 'Main container is not responding!'
+    log 'Main container is not responding!'
     # TODO Display logs to help bug fixing
     #echo 'Check the following logs for details:'
     #tail -n 100 logs/*.log
@@ -20,7 +24,7 @@ fi
 
 ################################################################################
 # Success
-echo 'Docker tests successful'
+log 'Docker tests successful'
 
 
 ################################################################################
@@ -39,14 +43,14 @@ if [ -n "${DOCKER_WEB_CONTAINER}" ]; then
         exit 2
     fi
 
-    DOCKER_WEB_CONTAINER_HEALTH_URL=http://${DOCKER_WEB_CONTAINER}:${DOCKER_WEB_PORT:-80}/${DOCKER_WEB_HEALTH_API:-conf.json}
-    log "Checking conf.json: ${DOCKER_WEB_CONTAINER_HEALTH_URL}"
-    curl --fail "${DOCKER_WEB_CONTAINER_HEALTH_URL}" | grep -q -e 'defaultTheme' || exit 1
+    DOCKER_WEB_CONTAINER_HEALTH_URL=http://${DOCKER_WEB_CONTAINER}:${DOCKER_WEB_PORT:-80}/${DOCKER_WEB_HEALTH_API:-health}
+    log "Checking Health API: ${DOCKER_WEB_CONTAINER_HEALTH_URL}"
+    curl --fail "${DOCKER_WEB_CONTAINER_HEALTH_URL}" | grep -q -e 'UP' || exit 1
 fi
 
 
 ################################################################################
 # Success
-echo "Docker app '${DOCKER_TEST_CONTAINER}' tests finished"
-echo 'Check the CI reports and logs for details.'
+log "Docker app '${DOCKER_TEST_CONTAINER}' tests finished"
+log 'Check the CI reports and logs for details.'
 exit 0
