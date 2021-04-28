47f61d3beca068e16ff45ef73eab56c57f749b92
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-04-28 21:29:57 +0200

    ⬆️ Upgrade Taiga 6
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ images/6.0/alpine/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+if [ ! -f /taiga/config.json ]; then
+  echo "Taiga events configuration missing!"
+  exit 1
+fi
+
+#########################################
+## Taiga Events config (legacy & 6.0+)
+#########################################
+
+if [ -n "${RABBIT_HOST}" ]; then
+  echo "Updating Taiga RabbitMQ URL: ${RABBIT_HOST}"
+  sed -i \
+    -e "s|\"url\": \".*\",|\"url\": \"amqp://${RABBIT_USER}:${RABBIT_PASSWORD}@${RABBIT_HOST}:${RABBIT_PORT}/${RABBIT_VHOST}\",|g" \
+    /taiga/config.json
+
+  export RABBITMQ_URL=amqp://${RABBIT_USER}:${RABBIT_PASSWORD}@${RABBIT_HOST}:${RABBIT_PORT}/${RABBIT_VHOST}
+fi
+
+if [ -n "${TAIGA_EVENTS_SECRET}" ]; then
+  echo "Updating Taiga Events secret (legacy): (don't expect to find those in the logs...)"
+  sed -i \
+    -e "s|\"secret\": \".*\",|\"secret\": \"${TAIGA_EVENTS_SECRET}\",|g" \
+    /taiga/config.json
+
+  export SECRET=${TAIGA_EVENTS_SECRET}
+fi
+
+if [ -n "${TAIGA_EVENTS_PORT}" ]; then
+  echo "Updating Taiga Events default port (legacy): ${TAIGA_EVENTS_PORT}"
+  sed -i \
+    -e "s|\"port\": \".*\",|\"port\": \"${TAIGA_EVENTS_PORT}\",|g" \
+    /taiga/config.json
+
+  export WEB_SOCKET_SERVER_PORT=${TAIGA_EVENTS_PORT}
+fi
+
+# Start coffee script
+exec "$@"
