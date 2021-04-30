e3d34cee451e057d18092bdf806c3ae959418282
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-05-01 00:46:05 +0200

    :wrench: Improve config and logs
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- template/entrypoint.sh
+++ template/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+log() {
+  echo "[$(date +%Y-%m-%dT%H:%M:%S%:z)] $*"
+}
+
 if [ ! -f /taiga/config.json ]; then
-  echo "Taiga events configuration missing!"
+  log "Taiga events configuration missing!"
   exit 1
 fi
 
@@ -10,7 +14,7 @@ fi
 #########################################
 
 if [ -n "${RABBIT_HOST:-$RABBITMQ_HOST}" ]; then
-  echo "Updating Taiga RabbitMQ URL: ${RABBIT_HOST:-$RABBITMQ_HOST}"
+  log "Updating Taiga RabbitMQ URL (legacy): ${RABBIT_HOST:-$RABBITMQ_HOST}"
   sed -i \
     -e "s|\"url\": \".*\",|\"url\": \"amqp://${RABBIT_USER:-$RABBITMQ_USER}:${RABBIT_PASSWORD:-$RABBITMQ_PASSWORD}@${RABBIT_HOST:-$RABBITMQ_HOST}:${RABBIT_PORT:-$RABBITMQ_PORT}${RABBIT_VHOST:-$RABBITMQ_VHOST}\",|g" \
     /taiga/config.json
@@ -19,7 +23,7 @@ if [ -n "${RABBIT_HOST:-$RABBITMQ_HOST}" ]; then
 fi
 
 if [ -n "${TAIGA_EVENTS_SECRET}" ]; then
-  echo "Updating Taiga Events secret (legacy): (don't expect to find those in the logs...)"
+  log "Updating Taiga Events secret (legacy): (don't expect to find those in the logs...)"
   sed -i \
     -e "s|\"secret\": \".*\",|\"secret\": \"${TAIGA_EVENTS_SECRET}\",|g" \
     /taiga/config.json
@@ -28,7 +32,7 @@ if [ -n "${TAIGA_EVENTS_SECRET}" ]; then
 fi
 
 if [ -n "${TAIGA_EVENTS_PORT}" ]; then
-  echo "Updating Taiga Events default port (legacy): ${TAIGA_EVENTS_PORT}"
+  log "Updating Taiga Events default port (legacy): ${TAIGA_EVENTS_PORT}"
   sed -i \
     -e "s|\"port\": \".*\",|\"port\": \"${TAIGA_EVENTS_PORT}\",|g" \
     /taiga/config.json
@@ -36,5 +40,6 @@ if [ -n "${TAIGA_EVENTS_PORT}" ]; then
   export WEB_SOCKET_SERVER_PORT=${TAIGA_EVENTS_PORT}
 fi
 
-# Start coffee script
+# Start
+log "Executing Taiga Events command"
 exec "$@"
